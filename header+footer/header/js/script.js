49958ebaaf3aee3dd34c77661bf2e663048da76b
peterkarnaukhsite <62539689+peterkarnaukhsite@users.noreply.github.com>
2020-11-06 13:08:40 +0200

    Add files via upload

---
+++ header+footer/header/js/script.js
@@ -0,0 +1,56 @@
+const searchButton = document.querySelector(".header__search"),
+  searchBox = document.querySelector(".header__searchbox"),
+  menuButton = document.querySelector(".menu-icon"),
+  body = document.querySelector('body'),
+  mobileMenu = document.querySelector('.header__mobilemenu');
+
+  
+let isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+  }
+};
+
+if (isMobile.any()) {
+  body.classList.add('touch');
+  let arrow = document.querySelectorAll('.arrow');
+  for (i = 0; i < arrow.length; i++) {
+		let thisLink = arrow[i].previousElementSibling;
+		let subMenu = arrow[i].nextElementSibling;
+		let thisArrow = arrow[i];
+
+		thisLink.classList.add('parent')
+		arrow[i].addEventListener('click', function () {
+			subMenu.classList.toggle('open');
+			thisArrow.classList.toggle('active');
+		});
+	}
+} else {
+	body.classList.add('mouse')
+}
+
+
+searchButton.addEventListener('click', function () {
+  searchBox.classList.toggle('active');
+});
+
+menuButton.addEventListener('click', function () {
+ this.classList.toggle('active');
+ mobileMenu.classList.toggle('active');
+});
+
